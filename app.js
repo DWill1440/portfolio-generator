f8078e0efdb763cf2577c08a039039fbec9debef
DWill1440 <64138572+DWill1440@users.noreply.github.com>
2020-07-14 12:23:38 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,11 @@ console.log(profileDataArgs);
 const printProfileData = profileDataArr => {
   for (let i = 0; i < profileDataArr.length; i += 1) {
     console.log(profileDataArr[i]);
-  }
-};
+    console.log('================');
 
-printProfileData(profileDataArgs);
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
-const printProfileData = profileDataArr => {
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
+profileDataArr.forEach(function(profileItem) {
+  console.log(profileItem);
+});
   }
 };
 
