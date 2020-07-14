09f3b515d5d483a387163f3b97a1f236649296e1
DWill1440 <64138572+DWill1440@users.noreply.github.com>
2020-07-14 13:12:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,7 @@
 // };
 
 // printProfileData(profileDataArgs);
+const profileDataArgs = process.argv.slice(2, process.argv.length);
 const generatePage = (userName, githubName) => {
   return `
     Name: ${userName}
