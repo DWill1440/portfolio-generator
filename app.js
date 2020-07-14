57c5366149e44d958c3587095d0bc2b866290d5a
DWill1440 <64138572+DWill1440@users.noreply.github.com>
2020-07-14 13:16:13 -0500

    Update app.js

--- app.js
+++ app.js
@@ -12,13 +12,13 @@
 
 // printProfileData(profileDataArgs);
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-const name = profileDataArgs[0];
-const github = profileDataArgs[1];
+const [name, github] = profileDataArgs;
 const generatePage = (userName, githubName) => {
   return `
     Name: ${userName}
     GitHub: ${githubName}
   `;
 };
+console.log(name, github);
 console.log(generatePage(name, github));
 
